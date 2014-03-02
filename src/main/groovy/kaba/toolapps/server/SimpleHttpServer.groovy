80948fbba394167e9862cf7af024adee315ec3d2
ryotan <ryotan@ucb-jp.net>
2014-03-03 02:22:08 +0900

    * HTTPサーバ部分は、Yokeを使ってみるようにしてみた。

--- src/main/groovy/kaba/toolapps/server/SimpleHttpServer.groovy
+++ src/main/groovy/kaba/toolapps/server/SimpleHttpServer.groovy
@@ -1,7 +1,14 @@
 package kaba.toolapps.server
-import org.vertx.groovy.core.http.HttpServer
-import org.vertx.groovy.core.http.HttpServerRequest
+import com.jetdrone.vertx.yoke.GYoke
+import com.jetdrone.vertx.yoke.middleware.BodyParser
+import com.jetdrone.vertx.yoke.middleware.ErrorHandler
+import com.jetdrone.vertx.yoke.middleware.Favicon
+import com.jetdrone.vertx.yoke.middleware.GRouter
+import com.jetdrone.vertx.yoke.middleware.Logger
+import com.jetdrone.vertx.yoke.middleware.Static
+import com.jetdrone.vertx.yoke.middleware.YokeRequest
 import org.vertx.groovy.platform.Verticle
+import org.vertx.java.core.Handler
 /**
  * シンプルなHTTPサーバ
  *
@@ -12,10 +19,25 @@ class SimpleHttpServer extends Verticle {
 
     @Override
     def start() {
-        HttpServer server = vertx.createHttpServer()
-        server.requestHandler { HttpServerRequest req ->
-            req.response.end("Hello World!!!")
-        }.listen(container.config.get("http.server.port") as int)
+        GYoke yoke = new GYoke(this)
+        yoke.use(new Logger())
+        yoke.use(new ErrorHandler(false))
+        yoke.use(new Favicon())
+        yoke.use('/static', new Static('.'))
+        yoke.use('/', new BodyParser())
+        yoke.use(
+            new GRouter().
+                get('/encrypt') { YokeRequest request, Handler<?> next ->
+                    request.response().end('Hello, Encrypted world!!!')
+                }.
+                get('/digest') { YokeRequest request, Handler<?> next ->
+                    request.response().end('Hello, Digesting world!!!')
+                }
+        )
+
+        int port = container.config['http.server.port'] as int
+        yoke.listen(port, "0.0.0.0")
+        container.logger.info("Vert.x http server is listening on 0.0.0.0:${port}")
     }
 
     @Override
