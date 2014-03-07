5a6f90c86ec3d436329432307317051e3554f472
ryotan <ryotan@ucb-jp.net>
2014-03-08 01:32:04 +0900

    * 存在しないアルゴリズムを指定されても、レスポンスは返すようにしてみた。 * コメントを追加。

--- src/main/groovy/kaba/toolapps/digest/DigestVerticle.groovy
+++ src/main/groovy/kaba/toolapps/digest/DigestVerticle.groovy
@@ -1,13 +1,12 @@
 package kaba.toolapps.digest
-
 import org.vertx.groovy.platform.Verticle
 import org.vertx.java.core.Handler
 import org.vertx.java.core.eventbus.Message
 
 import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
 import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.ConcurrentMap
-
 /**
  * SHA-256などのハッシュ化関数を使用して、ハッシュ値を計算するVerticle。
  *
@@ -16,6 +15,11 @@ import java.util.concurrent.ConcurrentMap
  */
 class DigestVerticle extends Verticle implements Handler<Message> {
 
+    /**
+     * {@link MessageDigest} のキャッシュ。
+     *
+     * マルチスレッドで動作しても問題ないように、 {@link ThreadLocal} に保持するようにしてみたんだけど。。。
+     */
     private static final ConcurrentMap<String, ThreadLocal<MessageDigest>> DIGESTS = new ConcurrentHashMap<>()
 
     @Override
@@ -27,13 +31,23 @@ class DigestVerticle extends Verticle implements Handler<Message> {
     void handle(Message event) {
         String body = event.body() as String
         if (body != null) {
-            byte[] digest1 = getDigest("sha-256").digest(body.getBytes())
-            event.reply(digest1.encodeBase64().toString())
+            try {
+                byte[] digest1 = getDigest("sha256").digest(body.getBytes())
+                event.reply(digest1.encodeBase64().toString())
+            } catch (NoSuchAlgorithmException e) {
+                event.reply("No Such Algorithm sha256. Cause: ${e.message}")
+            }
         }
         event.reply("")
     }
 
-    static MessageDigest getDigest(String algorithm) {
+    /**
+     * アルゴリズム名を指定して、 {@link MessageDigest} を取得する。
+     *
+     * @param algorithm {@link MessageDigest} のアルゴリズム名
+     * @return {@code algorithm} に対応する {@link MessageDigest}
+     */
+    private static MessageDigest getDigest(String algorithm) {
         ThreadLocal<MessageDigest> digest = DIGESTS.get(algorithm)
         if (digest != null) {
             return digest.get()
