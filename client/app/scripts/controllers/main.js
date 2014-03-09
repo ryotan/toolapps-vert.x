56c2efbe7541b925baa605ab2b721db32f1c9132
ryotan <ryotan@ucb-jp.net>
2014-03-09 13:58:56 +0900

    * 入力したらすぐにSHA-256で計算した値が表示されるよ！！

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -1,10 +1,19 @@
 'use strict';
 
 angular.module('toolappsClientApp')
-  .controller('MainCtrl', function($scope) {
+  .controller('MainCtrl', function($scope, $http) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    $http.get('/digest?target=<script></script>').success(function(data) {
+      $scope.raw = data.raw;
+      $scope.digest = data.digest;
+    });
+    $scope.getDigest = function() {
+      $http.get('/digest?target=' + $scope.raw).success(function(data) {
+        $scope.digest = data.digest;
+      });
+    }
   });
