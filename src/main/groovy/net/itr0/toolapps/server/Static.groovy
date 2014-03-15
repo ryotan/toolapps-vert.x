273508539d6d97963dee07a88f06b3df87646cf6
ryotan <ryotan@ucb-jp.net>
2014-03-16 02:20:25 +0900

    * cache-controlとdateヘッダを追加してみた。

--- src/main/groovy/net/itr0/toolapps/server/Static.groovy
+++ src/main/groovy/net/itr0/toolapps/server/Static.groovy
@@ -4,6 +4,9 @@ import com.jetdrone.vertx.yoke.Middleware
 import com.jetdrone.vertx.yoke.middleware.YokeRequest
 import org.vertx.java.core.Handler
 
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+
 /**
  * Static file server with the given {@code root} path.
  * When directory is requested without a file name being specified,
@@ -39,20 +42,36 @@ class Static extends Middleware {
      */
     int maxAge
 
+    private final DateFormat httpDateFormat
+
     Static(String root) {
         this(root, 'index.html', 315360000)
     }
 
     Static(String root, String index, int maxAge) {
+        // if the root is not empty it should end with / for convenience
+        if (!"".equals(root)) {
+            if (!root.endsWith("/")) {
+                root = root + "/";
+            }
+        }
+
         this.root = root
         this.index = index
         this.maxAge = maxAge
+
+        httpDateFormat = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z", Locale.US)
+        httpDateFormat.timeZone = TimeZone.getTimeZone("GMT")
     }
 
     @Override
     void handle(YokeRequest request, Handler<Object> next) {
         if (request.path() == '/') {
-            request.response().sendFile("${root}/index.html")
+            request.response().headers().add([
+                "date"         : httpDateFormat.format(new Date()),
+                "cache-control": "public, max-age=${maxAge}",
+            ])
+            request.response().sendFile("${root}index.html")
         } else {
             next.handle(null)
         }
